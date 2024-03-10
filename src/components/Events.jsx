f30608b06f3ff69ea2e0b3bff6de6971ac500d9c
ravirt7911 <ravirt7911@gmail.com>
2024-03-10 15:27:31 +0530

    last

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
-import './events.css'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { ReactComponent as RecentEvents } from '../assets/RECENT EVENTS.svg'
-import group1 from '../assets/events/Group 1.png'
-import group2 from '../assets/events/Group 2.png'
-import group3 from '../assets/events/Group 3.png'
-import group4 from '../assets/events/Group 4.png'
-import group5 from '../assets/events/Group 5.png'
-import group6 from '../assets/events/Group 6.png'
-import group7 from '../assets/events/Group 7.png'
-import group8 from '../assets/events/Group 8.png'
-import group9 from '../assets/events/Group 9.png'
-import { ReactComponent as Group } from '../assets/events/Group 1.svg'
-import { ReactComponent as Seemore } from '../assets/see more.svg'
+import React, { useState } from "react";
+import "./events.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { ReactComponent as RecentEvents } from "../assets/RECENT EVENTS.svg";
+import group1 from "../assets/events/Group 1.png";
+import group2 from "../assets/events/Group 2.png";
+import group3 from "../assets/events/Group 3.png";
+import group4 from "../assets/events/Group 4.png";
+import group5 from "../assets/events/Group 5.png";
+import group6 from "../assets/events/Group 6.png";
+import group7 from "../assets/events/Group 7.png";
+import group8 from "../assets/events/Group 8.png";
+import group9 from "../assets/events/Group 9.png";
+import { ReactComponent as Group } from "../assets/events/Group 1.svg";
+import { ReactComponent as Seemore } from "../assets/see more.svg";
 
 const Events = () => {
   const responsive = {
@@ -31,26 +31,40 @@ const Events = () => {
     },
   };
   return (
-   <section id="events" className="events">
-    <div className="recent-events-wrapper">
-          <RecentEvents className="recent-events" />
-    </div>
-    <div className="carousel"> 
-    {/* <Carousel responsive={responsive}> */}
-        {/* <Group className="group" /> */}
-        <a href='#'><img src={group1} alt="group1" className="events-image" /></a>
-        <a href='#'><img src={group2} alt="group2" className="events-image" /></a>
-        <a href='#'><img src={group3} alt="group3" className="events-image" /></a>
-        <a href='#'><img src={group4} alt="group4" className="events-image" /></a>
-        <a href='#'><img src={group5} alt="group5" className="events-image" /></a>
-        <a href='#'><img src={group6} alt="group6" className="events-image" /></a>
-        <a href='#'><img src={group7} alt="group7" className="events-image" /></a>
-        <a href='#'><img src={group8} alt="group8" className="events-image" /></a>
-        <a href='#'><img src={group9} alt="group9" className="events-image" /></a>
-      {/* </Carousel> */}
+    <section id="events" className="events">
+      <div className="recent-events-wrapper">
+        <RecentEvents className="recent-events" />
       </div>
-   </section>
-  )
-}
+      <div className="carousel">
+        {/* <Carousel responsive={responsive}> */}
+        {/* <Group className="group" /> */}
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-intro-to-rebase/cohost-vignans-institute-of-information-technology-visakhapatnam">
+          <img src={group1} alt="group1" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-from-gpt3-to-beyond-understanding-large-language-models/">
+          <img src={group2} alt="group2" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-android-accelerator-exploring-the-android-realm/">
+          <img src={group3} alt="group3" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-getting-started-with-flutter-building-your-first-app/">
+          <img src={group4} alt="group4" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-design-wonderland/">
+          <img src={group5} alt="group5" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-kalasalingam-academy-of-research-education-krishnankoil-presents-introduction-to-google-solution-challenge-2024/cohost-vignans-institute-of-information-technology-visakhapatnam">
+          <img src={group6} alt="group6" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-intro-to-open-source-amp-hands-on-git-amp-github-1/">
+          <img src={group7} alt="group7" className="events-image" />
+        </a>
+        <a href="https://gdsc.community.dev/events/details/developer-student-clubs-vignans-institute-of-information-technology-visakhapatnam-presents-inauguration-followed-by-a-session-on-flutter/">
+          <img src={group8} alt="group8" className="events-image" />
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Events
+export default Events;
